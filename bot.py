63b576fc9095aeff229dc0f4d367bad11f0cb9e4
Santhoshraj <santhoshraje95@gmail.com>
2020-06-26 23:21:00 +0800

    update: version info and added new background job
    - pulls data from website every 30 mins

--- bot.py
+++ bot.py
@@ -6,15 +6,19 @@ from Controllers.menu_controller import MenuController
 from Controllers.dc_controller import DCController
 from Controllers.ds_controller import DSController
 from Controllers.ud_controller import UDController
+#jobs
+from Kubera.tools import get_upcoming_dividends
 
 
 class Bot:
     def __init__(self):
-        log().info('Kubera v0.0.1 active')
+        log().info('Kubera v1.0 active')
         # loaded from config
         self.token = ''
         # telegram api
         self.updater = Updater(self.token, use_context=True)
+        # job queue
+        self.job_queue = self.updater.job_queue
         # Get the dispatcher to register handlers
         self.dp = self.updater.dispatcher
         # error handling
@@ -24,6 +28,8 @@ class Bot:
         DCController(self.dp)
         DSController(self.dp)
         UDController(self.dp)
+        # add jobs
+        self.job_queue.run_repeating(get_upcoming_dividends, interval=1800, first=0)
         # start bot
         self.updater.start_polling()
         self.updater.idle()
