889c9d4eba84f61481146b41d4e58e23da8dd832
Santhoshraj <santhoshraje95@gmail.com>
2020-06-26 23:34:37 +0800

    update: comment

--- utils/logging.py
+++ utils/logging.py
@@ -1,6 +1,6 @@
 import logging
 
-
+# logging function
 def get_logger():
     logger = logging.getLogger('log')
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
