726f3acdedd2da6fab957a68cd84638e2225e6c9
Santhoshraj <santhoshraje95@gmail.com>
2020-07-04 01:39:40 +0800

    update: write logs to file

--- Utils/logging.py
+++ Utils/logging.py
@@ -1,7 +1,10 @@
 import logging
+import sys
+
 
 # logging function
 def get_logger():
     logger = logging.getLogger('log')
+    logger.addHandler(logging.FileHandler("Logs/debug.txt"))
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     return logger
