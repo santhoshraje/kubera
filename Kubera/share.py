88be77bbebb951258de426b4ba0fe3ff2bf87729
Santhoshraj <santhoshraje95@gmail.com>
2020-07-04 01:40:52 +0800

    update: changed date format for dividend payout

--- Kubera/share.py
+++ Kubera/share.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from pandas_datareader import data
 from millify import millify
+from Utils.logging import get_logger as log
 
 
 class Share:
@@ -16,9 +17,9 @@ class Share:
         self.name = self.__name()
         self.price = self.__price()
         # dividend information
-        self.payout_amount = 'not available'
-        self.payout_date = 'not available'
-        self.yield_data = 'not available'
+        self.payout_amount = 'unavailable'
+        self.payout_date = 'unavailable'
+        self.yield_data = 'unavailable'
 
     # mode 1 = return string
     # mode 2 = return float
@@ -44,67 +45,71 @@ class Share:
         html = requests.get(url).text
         df = pd.read_html(html)[0]
         df.columns = df.columns.str.replace(' ', '')
+        # get data from page
         self.payout_amount = str(df.loc[df.Ticker == self.ticker_raw, 'Amount'].values[0])
-        self.payout_date = str(df.loc[df.Ticker == self.ticker_raw, 'NextDividend'].values[0])
+        # date = datetime.strptime(str(df.loc[df.Ticker == self.ticker_raw, 'NextDividend'].values[0]), '%Y-%m-%d')
+        # self.payout_date = date.strftime('%d %B %Y')
+        d = pd.to_datetime(str(df.loc[df.Ticker == self.ticker_raw, 'NextDividend'].values[0]))
+        self.payout_date = d.strftime('%d %B %Y')
         self.yield_data = str(df.loc[df.Ticker == self.ticker_raw, 'Yield'].values[0])
 
     def __str__(self):
         return 'Name:' + self.name + ' (' + self.ticker_raw + ')\nLatest price: SGD' + str(
             self.price) + '\nMarket Cap: ' + str(self.market_cap) + '\nBook Value Per Share (MRQ): SGD' + self.book_value \
-               + '\nPayout Amount: ' + self.payout_amount + '\nPayout date: ' + self.payout_date + '\n'
+               + '\nPayout Amount: ' + self.payout_amount + '\nPayout date: ' + self.payout_date + '\n Yield: ' + self.yield_data + '\n'
 
     def __data(self):
         try:
             tmp = data.get_quote_yahoo(self.ticker)
-        except:
-            print('ticker data not available')
+        except IndexError:
+            log().warning('ticker data not available for %s', self.ticker)
             tmp = None
 
         if tmp.empty:
-            return 'not available'
+            return 'unavailable'
 
         return tmp
 
     def __name(self):
         try:
             tmp = self.data['longName'].to_string(index=False)
-        except KeyError as e:
-            print(str(e) + 'key not available')
+        except KeyError:
+            log().warning('name not available for %s', self.ticker)
             tmp = None
 
         if not tmp:
-            return 'not available'
+            return 'unavailable'
         return tmp
 
     def __price(self):
         try:
             tmp = self.data['price'].to_string(index=False)
-        except KeyError as e:
-            print(str(e) + 'key not available')
+        except KeyError:
+            log().warning('price data not available for %s', self.ticker)
             tmp = None
 
         if not tmp:
-            return 'not available'
+            return 'unavailable'
         return tmp
 
     def __market_cap(self):
         try:
             tmp = millify(float(self.data['marketCap'].to_string(index=False)))
-        except KeyError as e:
-            print(str(e) + 'key not available')
+        except KeyError:
+            log().warning('market cap data not available for %s', self.ticker)
             tmp = None
 
         if not tmp:
-            return 'not available'
+            return 'unavailable'
         return tmp
 
     def __book_value(self):
         try:
             tmp = self.data['bookValue'].to_string(index=False)
-        except KeyError as e:
-            print(str(e) + 'key not available')
+        except KeyError:
+            log().warning('book value data not available for %s', self.ticker)
             tmp = None
 
         if not tmp:
-            return 'not available'
+            return 'unavailable'
         return tmp
