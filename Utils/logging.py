0cf460b31b6f9d802590a288948cc039d7c3d8b4
Santhoshraj <santhoshraje95@gmail.com>
2020-07-04 12:05:22 +0800

    fix: removed write logs to file
    bugs. unusable.

--- Utils/logging.py
+++ Utils/logging.py
@@ -5,6 +5,6 @@ import sys
 # logging function
 def get_logger():
     logger = logging.getLogger('log')
-    logger.addHandler(logging.FileHandler("Logs/debug.txt"))
+    # logger.addHandler(logging.FileHandler("Logs/debug.txt"))
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     return logger
