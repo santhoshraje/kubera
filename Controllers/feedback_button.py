7ab3d2e31244a61f715dc7641512d6b4459a62fd
Santhoshraj <santhoshraje95@gmail.com>
2020-07-09 10:17:06 +0800

    update: controller for feedback button
    - print user feedback in console
    - local states

---
+++ Controllers/feedback_button.py
@@ -0,0 +1,44 @@
+from telegram.ext import ConversationHandler
+from telegram.ext import CallbackQueryHandler
+from telegram.ext import Filters
+
+import Controllers.global_states as states
+
+from Utils.logging import get_logger as log
+
+from telegram.ext import MessageHandler
+
+GETFEEDBACK = range(1)
+
+class FeedbackButton:
+    def __init__(self, dispatcher):
+        self.__dp = dispatcher
+        self.__handler()
+
+    def __handler(self):
+        cancel_handler = ConversationHandler(
+            entry_points=[CallbackQueryHandler(self.__show_message, pattern='^' + str(states.FEEDBACK) + '$')],
+            states={
+                GETFEEDBACK: [
+                    # message handler
+                    MessageHandler(Filters.text, self.__store_feedback)
+                ]
+            },
+            fallbacks=[]
+        )
+        self.__dp.add_handler(cancel_handler)
+
+    def __show_message(self, update, context):
+        user = update.effective_user
+        query = update.callback_query
+        query.answer()
+        query.edit_message_text('Please enter your feedback')
+        return GETFEEDBACK
+
+    def __store_feedback(self, update, context):
+        feedback = update.message.text
+        user = update.effective_user
+        log().info("User %s entered feedback: %s", user.id, feedback)
+        update.message.reply_text("Your feedback has been recorded. Thank you!\nUse /start to go back to the main menu")
+        return ConversationHandler.END
+
