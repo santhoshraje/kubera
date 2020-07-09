f2e9fb654d352d52ea74089ce6e6af52ed42add4
Santhoshraj <santhoshraje95@gmail.com>
2020-07-09 10:10:56 +0800

    code cleanup

--- Utils/logging.py
+++ Utils/logging.py
@@ -1,10 +1,7 @@
 import logging
-import sys
 
 
-# logging function
 def get_logger():
     logger = logging.getLogger('log')
-    # logger.addHandler(logging.FileHandler("Logs/debug.txt"))
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     return logger
