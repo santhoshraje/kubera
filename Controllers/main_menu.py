634ede50849eb1b417f7bdec72fcc92fd785979b
Santhoshraj <santhoshraje95@gmail.com>
2020-07-09 10:09:55 +0800

    update: renamed files, removed view folder

--- Controllers/main_menu.py
+++ Controllers/main_menu.py
@@ -5,8 +5,10 @@ from telegram import InlineKeyboardMarkup
 
 import Controllers.states as states
 
+# utilities
 from Utils.logging import get_logger as log
-
+from Utils.check_user import does_user_exist as check
+# configuration
 from config import BotConfig
 
 
@@ -20,30 +22,26 @@ class MenuController:
                            "Dividends</b>\nDividend payouts that are coming soon.\n\n<b>Dividend Summary</b>" \
                            "\nDividends paid by a company over the last 5 years.\n\n<b>Dividend " \
                            "Calculator</b>\nCalculate your dividend payout.\n\n<b>Data " \
-                           "sources</b>:\ndividends.sg\nYahoo Finance\n\n Use /cancel to exit the menu."
+                           "sources</b>:\n<code>dividends.sg</code>\nYahoo Finance\n\n Use /cancel to exit the menu."
         self.__menu = None
 
+    # handlers
     def __handler(self):
         menu_handler = CommandHandler("start", self.__show_menu)
         self.__dp.add_handler(menu_handler)
 
+    def __cancel_handler(self):
+        cancel_handler = CommandHandler("cancel", self.__end_chat)
+        self.__dp.add_handler(cancel_handler)
+
+    # functions
     def __show_menu(self, update, context):
         user = update.effective_user
 
-        log().info("User %s started the conversation.", user.first_name)
-
-        user_id = str(user.id)
-
-        file = open("users.txt", "a+")
-        file.seek(0)  # set position to start of file
-        lines = file.read().splitlines()  # now we won't have those newlines
-
-        if user_id in lines:
-            log().info('user id already exists in file')
+        if check(str(user.id)):
+            log().info("New user %s started the conversation.", user.first_name)
         else:
-            # write to file
-            log().info('user id stored in file')
-            file.write("\n" + user_id)
+            log().info("Return user %s started the conversation.", user.first_name)
 
         keyboard = [
             [InlineKeyboardButton("🔸Upcoming Dividends",
@@ -51,17 +49,15 @@ class MenuController:
             [InlineKeyboardButton("🔸Dividend Summary",
                                   callback_data=str(states.DIVIDENDINFO))],
             [InlineKeyboardButton("🔸Dividend Calculator",
+                                  callback_data=str(states.DIVIDENDCALC))],
+            [InlineKeyboardButton("Cancel",
                                   callback_data=str(states.DIVIDENDCALC))]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         # Send message with text and appended InlineKeyboard
         self.__menu = update.message.reply_text(self.__menu_text, reply_markup=reply_markup, parse_mode='HTML')
 
-    def __cancel_handler(self):
-        cancel_handler = CommandHandler("cancel", self.__end_chat)
-        self.__dp.add_handler(cancel_handler)
-
     def __end_chat(self, update, context):
         user = update.effective_user
-        log().info("User %s ended the conversation.", user.id)
+        log().info("User %s ended the conversation.", user.first_name)
         self.__menu.edit_text('Chat ended. Use /start to show the menu again.')
