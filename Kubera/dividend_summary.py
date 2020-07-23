2c75ad140cdca4f27f692837332fb1e192ac6e88
Santhoshraj <santhoshraje95@gmail.com>
2020-07-24 00:03:03 +0800

    update: created dividend summary object
    holds general information for summary of dividends

---
+++ Kubera/dividend_summary.py
@@ -0,0 +1,9 @@
+class DividendSummary:
+    def __init__(self, year, total, amount, ex_date, pay_date):
+        # single values
+        self.year = year
+        self.total = total
+        # lists
+        self.amount = amount
+        self.ex_date = ex_date
+        self.pay_date = pay_date
