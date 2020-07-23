d22cbaa77bcc159ed452123090705af3bf0fe012
Santhoshraj <santhoshraje95@gmail.com>
2020-07-24 00:03:38 +0800

    update: added urls

--- config.py
+++ config.py
@@ -1,4 +1,19 @@
+from configparser import ConfigParser
+import os
+
+# get abs path
+folder = os.path.dirname(os.path.abspath(__file__))
+# get file path
+file = os.path.join(folder, 'api_key.ini')
+# read api key file
+config = ConfigParser()
+config.read(file)
+
+
 class BotConfig:
     def __init__(self):
         self.token = ''
         self.version = '1.0.3'
+        self.dividend_url = 'https://www.dividends.sg/view/'
+        self.upcoming_dividends_url = 'https://www.dividends.sg/dividend/coming'
+        self.admin_id = ''
