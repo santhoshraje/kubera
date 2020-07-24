3c7da1a0cea030f03fa06ecf17c338690ba040ba
Santhoshraj <santhoshraje95@gmail.com>
2020-07-24 18:56:40 +0800

    update: added support for external config file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # get abs path
 folder = os.path.dirname(os.path.abspath(__file__))
 # get file path
-file = os.path.join(folder, 'api_key.ini')
+file = os.path.join(folder, 'config.ini')
 # read api key file
 config = ConfigParser()
 config.read(file)
@@ -12,8 +12,8 @@ config.read(file)
 
 class BotConfig:
     def __init__(self):
-        self.token = ''
+        self.token = config.get('main', 'api_key')
         self.version = '1.1'
         self.dividend_url = 'https://www.dividends.sg/view/'
         self.upcoming_dividends_url = 'https://www.dividends.sg/dividend/coming'
-        self.admin_id = ''
+        self.admin_id = config.get('main', 'admin_id')
