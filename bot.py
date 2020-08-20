4a80b0dc8c0815e190518dd2261770f9f5dd69f6
Santhoshraj <santhoshraje95@gmail.com>
2020-08-20 14:18:04 +0800

    added mass update functionality to bot

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+# telegram api
 from telegram.ext import Updater
-
+# utils
+from Controllers.send_update import SendUpdate
 from Utils.logging import get_logger as log
-
+# controllers
 from Controllers.main_menu import MainMenu
 from Controllers.dividend_calculator import DividendCalculator
 from Controllers.dividend_summary import DividendSummary
@@ -40,6 +42,7 @@ class Bot:
         UpcomingDividends(self.dp)
         FeedbackButton(self.dp)
         CancelButton(self.dp)
+        SendUpdate(self.dp)
         # add jobs
         self.job_queue.run_repeating(get_upcoming_dividends, interval=3600, first=0)
         # start bot
