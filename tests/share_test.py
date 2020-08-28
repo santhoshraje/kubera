f1011ab9380c042a714ac78c5baafd4d77a1e6e7
Santhoshraj <santhoshraje95@gmail.com>
2020-08-28 15:46:28 +0800

    backtesting

--- tests/share_test.py
+++ tests/share_test.py
@@ -1,30 +1,50 @@
 from Kubera.share import Share
 import pandas as pd
+import pandas_datareader as pdr
+import datetime
 
 
 def main():
-    share = Share('qwac')
+    df = pd.read_csv('../data.csv')
+    tickers = df['Trading Code'].to_list()
+    pd.set_option('display.max_columns', None)
+    ibm = pdr.get_data_yahoo(symbols='IBM', start='2015-1-1', end='2015-12-31')
+    v = pdr.get_quote_yahoo()
 
-    a = share.get_dividend_summary(2020, 2015)
-    s = ''
+    print(ibm.columns.values)
 
-    if share.is_valid is False:
-        print('invalid ticker')
+    # for ticker in tickers:
+    #     share = Share(ticker)
+    #
+    #     # filter out penny stocks
+    #     if share.price > 1 and share.market_cap > 200000000:
+    #         if share.fifty_day_ma > share.two_hundred_day_ma:
+    #             print(share.name)
+    #             print(share.fifty_day_ma)
+    #             print(share.two_hundred_day_ma)
 
-    if a is None:
-        print('Dividend data not available')
-        return
-
-    for item in a:
-        s += str(item.year) + ' (' + str(item.total) + ')' + '\n'
-        i = 1
-        for pay_date, pay_amount in zip(item.pay_date, item.amount):
-            if pay_amount is '-':
-                continue
-            s += '‣ ' + pd.to_datetime(pay_date).strftime('%d %B') + ': ' + str(pay_amount).replace('SGD', 'SGD ') + '\n'
-            i += 1
-        s += '\n'
-    print(s)
+    # a = share.get_dividend_summary(2020, 2015)
+    # s = ''
+    #
+    # if share.is_valid is False:
+    #     print('invalid ticker')
+    #
+    # if a is None:
+    #     print('Dividend data not available')
+    #     return
+    #
+    # for item in a:
+    #     s += str(item.year) + ' (' + str(item.total) + ')' + '\n'
+    #     i = 1
+    #     for pay_date, pay_amount in zip(item.pay_date, item.amount):
+    #         if pay_amount is '-':
+    #             continue
+    #         s += '‣ ' + pd.to_datetime(pay_date).strftime('%d %B') + ': ' + str(pay_amount).replace('SGD', 'SGD ') + '\n'
+    #         i += 1
+    #     s += '\n'
+    # print(s)
+    # print(share.fifty_day_ma)
+    # print(share.two_hundred_day_ma)
     # s.get_data(2019)
     # s.get_dividend_breakdown(2019)
     # dividends = s.get_dividend_payout(datetime.datetime.now().year, datetime.datetime.now().year - 5)
