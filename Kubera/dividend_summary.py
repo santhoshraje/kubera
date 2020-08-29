61eec03301655f1f9cd6b2a433e3e70a145aae9a
Santhoshraj <santhoshraje95@gmail.com>
2020-08-29 18:37:49 +0800

    added printing of object

--- Kubera/dividend_summary.py
+++ Kubera/dividend_summary.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class DividendSummary:
     def __init__(self, year, total, amount, ex_date, pay_date):
         # single values
@@ -7,3 +10,15 @@ class DividendSummary:
         self.amount = amount
         self.ex_date = ex_date
         self.pay_date = pay_date
+
+    def __str__(self):
+        s = ''
+        s += str(self.year) + ' (' + str(self.total) + ')' + '\n'
+        i = 1
+        for pay_date, pay_amount in zip(self.pay_date, self.amount):
+            if pay_date is '-':
+                continue
+            s += '‣ ' + pd.to_datetime(pay_date).strftime('%d %B') + ': ' + str(pay_amount).replace('SGD',
+                                                                                                    'SGD ') + '\n'
+            i += 1
+        return s
