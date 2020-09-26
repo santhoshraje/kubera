636fd24930f01996b6c4697d2c4a5aa54049d44e
Santhoshraj <santhoshraje95@gmail.com>
2020-09-26 19:38:26 +0800

    formatting changes, removed unused import

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from config import BotConfig
 #db
 from db_engine import DBEngine
 # tools
-import datetime, pytz
+import datetime
 
 
 class Bot:
@@ -27,7 +27,6 @@ class Bot:
         log().info('kubera version ' + self.config.version + ' active')
         # db engine
         db = DBEngine()
-        # db.setup()
         # loaded from config
         self.token = self.config.token
         # telegram api
@@ -38,7 +37,7 @@ class Bot:
         self.dp = self.updater.dispatcher
         # error handling
         self.dp.add_error_handler(self.error)
-        # add controllers
+        # controllers
         MainMenu(self.dp)
         DividendCalculator(self.dp)
         DividendSummary(self.dp)
@@ -46,10 +45,10 @@ class Bot:
         FeedbackButton(self.dp)
         CancelButton(self.dp)
         SendUpdate(self.dp)
-        # add jobs
+        # jobs
         self.job_queue.run_repeating(get_upcoming_dividends, interval=3600, first=0)
-        dt = datetime.time(hour=9, minute=1) # 5:01 PM singapore time (after market close)
-        self.job_queue.run_daily(post_market_analysis, dt)
+        # 5:01 PM singapore time (after market close)
+        self.job_queue.run_daily(post_market_analysis, datetime.time(hour=9, minute=00))
         # start bot
         self.updater.start_polling()
         self.updater.idle()
