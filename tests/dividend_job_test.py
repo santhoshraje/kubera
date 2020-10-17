d6ce189035a973c1807bc80e253c787f00db303b
Santhoshraj <santhoshraje95@gmail.com>
2020-10-17 14:55:57 +0800

    updated test

--- tests/dividend_job_test.py
+++ tests/dividend_job_test.py
@@ -1,40 +1,37 @@
 import pandas as pd
 import requests
 from Kubera.share import Share
-import pickle
-import numpy as np
+from db_engine import DBEngine
 
 from config import BotConfig
-
+import numpy as np
 
 def main():
-    array = []
     url = BotConfig().upcoming_dividends_url
     html = requests.get(url).text
     df = pd.read_html(html)[0]
     df.columns = df.columns.str.replace(' ', '')
+    db = DBEngine()
 
     tickers = df['Ticker'].tolist()
 
     for ticker in tickers:
         share = Share(ticker)
-        print(share.name)
         if not share.is_valid:
             continue
-        share.payout_amount = str(df.loc[df.Ticker == ticker, 'Amount'].values[0])
-        share.payout_date = pd.to_datetime(str(df.loc[df.Ticker == ticker, 'NextDividend'].values[0])).strftime(
-            '%d %B %Y')
-        share.yield_data = str(df.loc[df.Ticker == ticker, 'Yield'].values[0])
-        array.append(share)
+        name = share.name
+        ticker = share.ticker
+        market_cap = share.market_cap
+        price = str(share.price)
+        payout = str(df.loc[df.Ticker == ticker, 'Amount'].values[0])
+        date = pd.to_datetime(str(df.loc[df.Ticker == ticker, 'NextDividend'].values[0])).strftime('%d %B %Y')
+        yield_value = str(df.loc[df.Ticker == ticker, 'Yield'].values[0])
+        # db.add_item('dividends', ['name', 'ticker', 'market_cap', 'price', 'payout', 'yield', 'date'],
+        #             [name, ticker, market_cap, price, payout, yield_value, date])
 
-    tmp = np.array_split(array, 5)
-    file_count = 1
-
-    for x in tmp:
-        f = open("upcoming" + str(file_count) + ".pickle", 'wb')
-        pickle.dump(x, f)
-        f.close()
-        file_count += 1
+    rows = db.get_items('dividends', '*')
+    for row in rows:
+        print(row)
 
 
 main()
