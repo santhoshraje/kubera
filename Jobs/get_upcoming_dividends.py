acd5787579349e0cc09306a6e2fffff7346270eb
Santhoshraj <santhoshraje95@gmail.com>
2020-10-19 20:53:20 +0800

    added hash check to prevent polling data source too frequently

--- Jobs/get_upcoming_dividends.py
+++ Jobs/get_upcoming_dividends.py
@@ -2,18 +2,35 @@ import pandas as pd
 import requests
 from Kubera.share import Share
 import pickle
-import numpy as np
 import telegram.ext
 from config import BotConfig
 from db_engine import DBEngine
+from pandas.util import hash_pandas_object
 
 
 def get_upcoming_dividends(context: telegram.ext.CallbackContext):
-    url = BotConfig().upcoming_dividends_url
-    html = requests.get(url).text
-    df = pd.read_html(html)[0]
+    page = requests.get(BotConfig().upcoming_dividends_url).text
+    df = pd.read_html(page)[0]
+    hash_value = hash_pandas_object(df).sum()
+    # hash check
+    try:
+        with open('hash.pickle', 'rb') as hash_file:
+            b = pickle.load(hash_file)
+        # check if latest hash value matches with old one
+        if b == hash_value:
+            return
+        else:
+            # update hash value in the local file
+            with open('hash.pickle', 'wb') as hash_file:
+                pickle.dump(hash_value, hash_file)
+    # no hash file found
+    except FileNotFoundError:
+        with open('hash.pickle', 'wb') as hash_file:
+            pickle.dump(hash_value, hash_file)
+
     df.columns = df.columns.str.replace(' ', '')
     db = DBEngine()
+    db.clear_table('dividends')
 
     tickers = df['Ticker'].tolist()
 
@@ -28,5 +45,7 @@ def get_upcoming_dividends(context: telegram.ext.CallbackContext):
         payout = str(df.loc[df.Ticker == ticker, 'Amount'].values[0])
         date = pd.to_datetime(str(df.loc[df.Ticker == ticker, 'NextDividend'].values[0])).strftime('%d %B %Y')
         yield_value = str(df.loc[df.Ticker == ticker, 'Yield'].values[0])
-        db.add_item('dividends', ['name', 'ticker', 'market_cap', 'price', 'payout', 'yield', 'date'], [name, ticker, market_cap, price, payout, yield_value, date])
+
+        db.add_item('dividends', ['name', 'ticker', 'market_cap', 'price', 'payout', 'yield', 'date'],
+                    [name, ticker, market_cap, price, payout, yield_value, date])
 
--- tests/dividend_job_test.py
+++ tests/dividend_job_test.py
@@ -2,16 +2,34 @@ import pandas as pd
 import requests
 from Kubera.share import Share
 from db_engine import DBEngine
-
 from config import BotConfig
-import numpy as np
+from pandas.util import hash_pandas_object
+import pickle
+
 
 def main():
-    url = BotConfig().upcoming_dividends_url
-    html = requests.get(url).text
-    df = pd.read_html(html)[0]
+    page = requests.get(BotConfig().upcoming_dividends_url).text
+    df = pd.read_html(page)[0]
+    hash_value = hash_pandas_object(df).sum()
+    # hash check
+    try:
+        with open('hash.pickle', 'rb') as hash_file:
+            b = pickle.load(hash_file)
+        # check if latest hash value matches with old one
+        if b == hash_value:
+            return
+        else:
+            # update hash value in the local file
+            with open('hash.pickle', 'wb') as hash_file:
+                pickle.dump(hash_value, hash_file)
+    # no hash file found
+    except FileNotFoundError:
+        with open('hash.pickle', 'wb') as hash_file:
+            pickle.dump(hash_value, hash_file)
+
     df.columns = df.columns.str.replace(' ', '')
     db = DBEngine()
+    db.clear_table('dividends')
 
     tickers = df['Ticker'].tolist()
 
@@ -26,10 +44,12 @@ def main():
         payout = str(df.loc[df.Ticker == ticker, 'Amount'].values[0])
         date = pd.to_datetime(str(df.loc[df.Ticker == ticker, 'NextDividend'].values[0])).strftime('%d %B %Y')
         yield_value = str(df.loc[df.Ticker == ticker, 'Yield'].values[0])
-        # db.add_item('dividends', ['name', 'ticker', 'market_cap', 'price', 'payout', 'yield', 'date'],
-        #             [name, ticker, market_cap, price, payout, yield_value, date])
+
+        db.add_item('dividends', ['name', 'ticker', 'market_cap', 'price', 'payout', 'yield', 'date'],
+                    [name, ticker, market_cap, price, payout, yield_value, date])
 
     rows = db.get_items('dividends', '*')
+
     for row in rows:
         print(row)
 
