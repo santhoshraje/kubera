967877e219d243ffb8c7fb87b66aca98d46790aa
Santhoshraj <38834149+santhoshraje@users.noreply.github.com>
2021-03-02 11:19:03 +0800

    config updates

--- config.py
+++ config.py
@@ -16,8 +16,11 @@ class BotConfig:
             self.token = config.get('main', 'dev_api_key')
         else:
             self.token = config.get('main', 'prod_api_key')
+        # version info
         self.version = '1.3'
-        self.dividend_url = 'https://www.dividends.sg/view/'
-        self.upcoming_dividends_url = 'https://www.dividends.sg/dividend/coming'
+        # data sources
+        self.dividend_url = config.get('main', 'dividend_url')
+        self.upcoming_dividends_url = config.get('main', 'upcoming_dividends_url')
+        # admin login info
         self.admin_id = config.get('main', 'admin_id')
         self.admin_password = config.get('main', 'admin_password')
