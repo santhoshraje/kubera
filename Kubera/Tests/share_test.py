0ab189a54db3b5071d7bd542e124338cbfb9afa2
santhosh <santhoshraje95@gmail.com>
2021-10-29 12:33:53 +0800

    Update share_test.py
    more tests

--- Kubera/Tests/share_test.py
+++ Kubera/Tests/share_test.py
@@ -5,17 +5,24 @@ import datetime
 
 
 def main():
-    share = Share('d05')
+    share = Share('v03')
     # check if share is valid
-    if not share.is_valid:
-        print('invalid share')
-        return
-    # dividend summary test
-    a = share.get_dividend_summary(datetime.datetime.now().year, datetime.datetime.now().year - 5)
-    # DS object print test
-    for item in a:
-        print(item)
+    # if not share.is_valid:
+    #     print('invalid share')
+    #     return
+    # # dividend summary test
+    # a = share.get_dividend_summary(datetime.datetime.now().year, datetime.datetime.now().year - 5)
+    # # DS object print test
+    # for item in a:
+    #     print(item)
+    print(share.price)
+    print(share.open)
+    print(share.volume)
+    print(share.low)
+    print(share.high)
+    print(share.previous_close)
     print(share.fifty_day_ma)
+
     # print(share.two_hundred_day_ma)
 
 
